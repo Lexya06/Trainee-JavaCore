5b92796a2eff172bf7b9b627b27faf3d59533c76
Lexya <arikhartmen75@gmail.com>
2025-10-14 14:17:05 +0300

    Skynet complete

---
+++ Skynet/src/main/java/com/github/lexya/FactoryBattle.java
@@ -0,0 +1,57 @@
+package com.github.lexya;
+import static com.github.lexya.Skynet.*;
+import java.util.concurrent.Phaser;
+
+public class FactoryBattle {
+    static final int DAYS = 100;
+    static String[] factionNames = {"Wednesday","World"};
+    static Faction[] factions = new Faction[factionNames.length];
+    static Thread[] threadFactions = new Thread[factions.length];
+    static final Phaser phaser = new Phaser(factions.length + 1);
+    public static void main(String[] args) {
+         for (int j = 0; j < factions.length; j++) {
+            factions[j] = new Faction(factionNames[j]);
+        }
+
+         // starting threads for factions and waiting for factory generation details
+         for (int i = 0; i < factions.length; i++) {
+            Faction f = factions[i];
+            threadFactions[i] = new Thread(() -> {
+                for (int day = 0; day < DAYS; day++) {
+                    phaser.arriveAndAwaitAdvance();
+                    f.carryDetailsAndGetRobots();
+                }
+            });
+            threadFactions[i].start();
+        }
+
+        for (int day = 0; day < DAYS; day++) {
+            Factory.GenerateDetailsToDivide();
+            // move to next phase
+            phaser.arriveAndAwaitAdvance();
+        }
+
+        for (Thread t : threadFactions) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+
+        int maxRobots = 0;
+        int factionWithMaxArmyNumber = 0;
+        for (int i = 0; i < factions.length; i++) {
+            if (factions[i].robots > maxRobots) {
+                maxRobots = factions[i].robots;
+                factionWithMaxArmyNumber = i;
+            }
+            System.out.println("Faction " + factions[i].getName() + " has "+factions[i].robots+" robots");
+        }
+        System.out.println("Faction with max army number is "+factions[factionWithMaxArmyNumber].getName()+
+                ",army: "+maxRobots + " robots");
+
+
+    }
+}
---
+++ Skynet/src/main/java/com/github/lexya/Skynet.java
@@ -0,0 +1,113 @@
+package com.github.lexya;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Skynet {
+    static final ReentrantLock detailsLock = new ReentrantLock(true);
+    public enum Details {
+        HEAD,
+        TORSO,
+        HANDS,
+        FEET
+    }
+    static int[] detailsToDivide = new int [Details.values().length];
+
+    public static class Factory{
+        static int maxDetails = 10;
+        public static void GenerateDetailsToDivide(){
+            detailsLock.lock();
+            getRandomDetails(detailsToDivide, maxDetails + 1, maxDetails, 10);
+            detailsLock.unlock();
+        }
+
+
+    }
+
+    private static void getRandomDetails(int[] details, int maxDetails, int getFromValue,int iterations){
+        int detailsCount = 0;
+        while (iterations > 0 && detailsCount < maxDetails) {
+            int detailType = (int) (Math.random() * Details.values().length);
+            int added = (int) ((Math.random() * getFromValue) % (maxDetails -  detailsCount));
+            details[detailType] += added;
+            detailsCount += added;
+            iterations--;
+        }
+    }
+
+
+
+
+
+
+    public static class Faction{
+        static int maxDetailsCarry = 5;
+        int[] robotsDetails;
+        int robots;
+
+        private final String name;
+        public Faction(String name){
+            this.name = name;
+            robots = 0;
+            robotsDetails = new int[Details.values().length];
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        private static void getRandomRobotsDetails(int[] details, int[] divideDetails){
+            int detailsCount = 0;
+            for (int i = 0; i < divideDetails.length; i++) {
+                int temp = details[i];
+                getRandomDetails(details, maxDetailsCarry + 1 - detailsCount, divideDetails[i],1);
+                int diff = details[i]-temp;
+                detailsCount+=diff;
+                divideDetails[i] -= diff;
+            }
+        }
+
+        private static Details getNeededPartsType(int indDetail1, int indDetail2, int[] arr){
+            int minInd;
+
+            int amountOne = arr[indDetail1];
+            int amountTwo = arr[indDetail2];
+            if (amountOne < amountTwo) {
+                minInd = indDetail1;
+            }
+            else{
+                minInd = indDetail2;
+            }
+            return Details.values()[minInd];
+        }
+
+        private void updateDetails(int singlePartsAmount, int doublePartsAmount){
+            robotsDetails[Details.HEAD.ordinal()]-=singlePartsAmount;
+            robotsDetails[Details.TORSO.ordinal()]-=singlePartsAmount;
+            robotsDetails[Details.FEET.ordinal()]-=doublePartsAmount;
+            robotsDetails[Details.HANDS.ordinal()]-=doublePartsAmount;
+        }
+
+
+        public void carryDetailsAndGetRobots(){
+            detailsLock.lock();
+            getRandomRobotsDetails(robotsDetails, detailsToDivide);
+            detailsLock.unlock();
+
+
+            Details singlePart = getNeededPartsType(Details.HANDS.ordinal(),Details.TORSO.ordinal(),robotsDetails);
+            Details doublePart = getNeededPartsType(Details.HANDS.ordinal(), Details.FEET.ordinal(),robotsDetails);
+
+            int singlePartAmount = robotsDetails[singlePart.ordinal()];
+            int doublePartAmount = robotsDetails[doublePart.ordinal()];
+
+            if (doublePartAmount == 0 || singlePartAmount == 0){
+                    return;
+            }
+            int pairsParts = doublePartAmount / 2;
+            robots += Math.min(pairsParts, singlePartAmount);
+
+            updateDetails(singlePartAmount, pairsParts * 2);
+        }
+
+    }
+}
